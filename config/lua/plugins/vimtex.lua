91c219f0197f9c8b472f6838e8610baec06266b7
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-10 20:57:06 +0000

    fix

--- config/lua/plugins/vimtex.lua
+++ config/lua/plugins/vimtex.lua
@@ -6,10 +6,11 @@ return {
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = "sioyek"
     vim.g.vimtex_view_sioyek_options = string.format(
-    '--inverse-search "NVIM_LISTEN_ADDRESS=%s nvim --headless -c \\"VimtexInverseSearch %%2 \'%%1\'\\""',
-    vim.fn.serverlist()[1]
+      '--inverse-search "NVIM_LISTEN_ADDRESS=%s nvim --headless -c \\"VimtexInverseSearch %%2 \'%%1\'\\""',
+      vim.fn.serverlist()[1]
+    )
 
-    -- move by visual line inside tex files
+    -- move by visual line inside tex and bib files
     vim.api.nvim_create_autocmd("FileType", {
       pattern = { "tex", "bib" },
       callback = function()
@@ -17,6 +18,5 @@ return {
         vim.keymap.set("n", "k", "gk", { buffer = true })
       end,
     })
-    )
   end,
 }
