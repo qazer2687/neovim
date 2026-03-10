da6cad8182ae3c094c3a58c3b1ba0453a507401b
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-10 20:50:26 +0000

    fix

--- config/lua/plugins/vimtex.lua
+++ config/lua/plugins/vimtex.lua
@@ -8,6 +8,15 @@ return {
     vim.g.vimtex_view_sioyek_options = string.format(
     '--inverse-search "NVIM_LISTEN_ADDRESS=%s nvim --headless -c \\"VimtexInverseSearch %%2 \'%%1\'\\""',
     vim.fn.serverlist()[1]
+
+    -- move by visual line inside tex files
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "tex", "bib" },
+      callback = function()
+        vim.keymap.set("n", "j", "gj", { buffer = true })
+        vim.keymap.set("n", "k", "gk", { buffer = true })
+      end,
+    })
     )
   end,
 }
