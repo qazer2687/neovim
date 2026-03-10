7b33db1d258f13338da2635c7eec84f95c7bbbcd
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-10 20:34:19 +0000

    test

--- config/init.lua
+++ config/init.lua
@@ -84,7 +84,7 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
-vim.fn.serverstart("/tmp/nvim.sock")
+--vim.fn.serverstart("/tmp/nvim.sock")
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
