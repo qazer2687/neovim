1f56b06aba70dec199799506179c46cd36d81047
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-10 21:28:14 +0000

    fix

--- config/lua/options.lua
+++ config/lua/options.lua
@@ -60,9 +60,11 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 999
 
 -- Configure background.
 vim.opt.background = "dark"
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 -- vim: ts=2 sts=2 sw=2 et
