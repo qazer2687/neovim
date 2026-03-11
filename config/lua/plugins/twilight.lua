859e4344cb21274c9a1ce1dd9519f92ac3a9a0a9
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-11 01:13:02 +0000

    sync

--- config/lua/plugins/twilight.lua
+++ config/lua/plugins/twilight.lua
@@ -1,6 +1,13 @@
 return {
   "folke/twilight.nvim",
-  opts = {},
+  opts = {
+    context = 0,       -- no extra lines beyond the current treesitter node
+    treesitter = true,
+    expand = {
+      "function",
+      "method",
+    },
+  },
   init = function()
     vim.api.nvim_create_autocmd("VimEnter", {
       callback = function()
