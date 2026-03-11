6d9977fcb7c45b14cf0706d4a0ee9f94adad9744
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-11 02:24:25 +0000

    test

--- config/lua/plugins/twilight.lua
+++ config/lua/plugins/twilight.lua
@@ -8,11 +8,4 @@ return {
       "method",
     },
   },
-  init = function()
-    vim.api.nvim_create_autocmd("VimEnter", {
-      callback = function()
-        vim.cmd("Twilight")
-      end,
-    })
-  end,
 }
--- config/lua/plugins/zen-mode.lua
+++ config/lua/plugins/zen-mode.lua
@@ -14,7 +14,6 @@ return {
         list = false,
         wrap = true,
         linebreak = true,
-        scrolloff = 999,
       },
     },
     plugins = {
