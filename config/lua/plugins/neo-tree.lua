87e26ecf042f69c1fde4d1e7b76be9ebc230e7e2
qazer2687 <114782572+qazer2687@users.noreply.github.com>
2026-03-11 01:54:38 +0000

    fix

--- config/lua/plugins/neo-tree.lua
+++ config/lua/plugins/neo-tree.lua
@@ -16,6 +16,9 @@ return {
       statusline = false,
     },
   },
+
+  vim.api.nvim_set_hl(0, "NeoTreeStatusLine", { fg = "NONE", bg = "NONE" })
+  vim.api.nvim_set_hl(0, "NeoTreeStatusLineNC", { fg = "NONE", bg = "NONE" })
   -- Display neotree on launch.
   config = function()
     vim.schedule(function()
